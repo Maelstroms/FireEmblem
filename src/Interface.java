eb912c0c5c068d03a56ccccf8df5b813326a62fe
Maelstroms <workpartystudy@yahoo.com>
2014-12-10 15:48:25 -0500

    inital classes

---
+++ src/Interface.java
@@ -0,0 +1,5 @@
+/**
+ * Created by grantmerrill on 12/10/14.
+ */
+public class Interface {
+}
---
+++ src/Units.java
@@ -0,0 +1,16 @@
+/**
+ * Created by grantmerrill on 12/9/14.
+ *
+ * Abstract class to allow other parts of teh game to quickly inherit characteristics
+ */
+public class Units {
+
+    protected int health;
+    protected int location;
+    protected int attack;
+    protected int defense;
+    protected int speed;
+    protected int range;
+    protected int xp;
+
+}
